f4c52c8954ddb32bc193a6276f57a326b2690b8d
dhillweb <danielhill@magenta.de>
2024-09-13 09:22:07 +0200

    minimal setup

--- app.vue
+++ app.vue
@@ -10,6 +10,31 @@ useHead({
 </script>
 <template>
   <div>
+    <Html lang="en">
+
+    <Head>
+      <Title></Title>
+      <Meta name="description"
+        :content="`Your ultimate destination for design, development, and consulting. I transform your visions into reality and go beyond your expectations. Reach out and let’s create something amazing together!`" />
+      <Meta name="twitter:card" :content="`summary`" />
+      <Meta name="twitter:title" :content="`Innovative Designer and Developer | Custom Solutions Crafted for You`" />
+      <Meta name="twitter:description"
+        :content="`Your ultimate destination for design, development, and consulting. I transform your visions into reality and go beyond your expectations. Reach out and let’s create something amazing together!`" />
+      <Meta name="title" :content="`Innovative Designer and Developer | Custom Solutions Crafted for You`" />
+      <Meta name="description"
+        :content="`Your ultimate destination for design, development, and consulting. I transform your visions into reality and go beyond your expectations. Reach out and let’s create something amazing together!`" />
+      <Meta name="robots" :content="`index, follow`" />
+      <Meta property="og:type" :content="`website`" />
+      <Meta property="og:title" :content="`Innovative Designer and Developer | Custom Solutions Crafted for You`" />
+      <Meta property="og:description"
+        :content="`Your ultimate destination for design, development, and consulting. I transform your visions into reality and go beyond your expectations. Reach out and let’s create something amazing together!`" />
+      <Meta property="og:url" :content="`danielhill.nuxt.space`" />
+      <Meta property="og:image"
+        :content="`https://repository-images.githubusercontent.com/856030104/2a946d9b-8135-4327-bdbf-82b873ec10d2`" />
+
+    </Head>
+
+    </Html>
 
 
     <div
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,5 @@
 <template>
-    <Html lang="en">
 
-    <Head>
-        <Title>Home</Title>
-        <Meta name="description"
-            :content="`Your ultimate destination for design, development, and consulting. I transform your visions into reality and go beyond your expectations. Reach out and let’s create something amazing together!`" />
-        <Meta name="twitter:card" :content="`summary`" />
-        <Meta name="twitter:title" :content="`Innovative Designer and Developer | Custom Solutions Crafted for You`" />
-        <Meta name="twitter:description"
-            :content="`Your ultimate destination for design, development, and consulting. I transform your visions into reality and go beyond your expectations. Reach out and let’s create something amazing together!`" />
-        <Meta name="title" :content="`Innovative Designer and Developer | Custom Solutions Crafted for You`" />
-        <Meta name="description"
-            :content="`Your ultimate destination for design, development, and consulting. I transform your visions into reality and go beyond your expectations. Reach out and let’s create something amazing together!`" />
-        <Meta name="robots" :content="`index, follow`" />
-        <Meta property="og:type" :content="`website`" />
-        <Meta property="og:title" :content="`Innovative Designer and Developer | Custom Solutions Crafted for You`" />
-        <Meta property="og:description"
-            :content="`Your ultimate destination for design, development, and consulting. I transform your visions into reality and go beyond your expectations. Reach out and let’s create something amazing together!`" />
-        <Meta property="og:url" :content="`danielhill.nuxt.space`" />
-        <Meta property="og:image"
-            :content="`https://repository-images.githubusercontent.com/856030104/2a946d9b-8135-4327-bdbf-82b873ec10d2`" />
-
-    </Head>
-
-    </Html>
     <p class="pb-20">
         Welcome to Daniel Hill's digital space, where he shares insights into his work and passions. Based in Thuringia,
         Germany, Daniel is a multimedia designer, software developer, and tech enthusiast. Since 1999, he has honed his
@@ -55,4 +31,8 @@
 
 const article = { limit: 4, sort: [{ date: -1 }] }
 
+useSeoMeta({
+    title: "Home",
+    description: "Welcome to Daniel Hill's digital space.",
+})
 </script>
