ffc418886c20582495789f7dd87b415732f4f681
dhillweb <danielhill@magenta.de>
2024-09-18 01:38:02 +0200

    final

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
     head: {
       link: [
         {
-          rel: 'preload',
+          rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap'
         }
       ]
