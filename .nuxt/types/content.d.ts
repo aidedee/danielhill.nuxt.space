7c55aa7a91c9adc4c51920c6692a6cb19d9650ce
dhillweb <danielhill@magenta.de>
2024-09-21 02:00:32 +0000

    Deploying to gh-pages from @ dhillweb/danielhill.nuxt.space@b54b16af493e2bfe061c6ce61d7febd1cb562c1a 🚀

--- .nuxt/types/content.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '#content/server' {
-  const serverQueryContent: typeof import('C:/Users/danielhill/Desktop/GitHub/danielhill.nuxt.space/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
-  const parseContent: typeof import('C:/Users/danielhill/Desktop/GitHub/danielhill.nuxt.space/node_modules/@nuxt/content/dist/runtime/server').parseContent
-}
